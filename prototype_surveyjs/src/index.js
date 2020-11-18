a29a6b487be3196dda97d66cfa60ca5409df6c94
LegatusNewt <kennywu1005@gmail.com>
2020-11-18 12:24:53 -0700

    Really Basic survey js with react. SurveyJS can be easily moved to a functional component, if someone prefers class components we can do that as well.

---
+++ prototype_surveyjs/src/index.js
@@ -0,0 +1,10 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+import SurveyComponent from './SurveyComponent';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <SurveyComponent />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
