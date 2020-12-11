21c11145dc8a50c12528bafd025e64890fe00e46
jdhgit <17481018+jdhern17@users.noreply.github.com>
2020-12-11 11:21:33 -0700

    added preliminary questions and began secondary

--- src/data/survey.js
+++ src/data/survey.js
@@ -1,5 +1,5 @@
 const SURVEY_DATA = {
-    title: 'Tell us, what technologies do you use?',
+    title: 'Tell us some more about your case.',
     pages: [
         {
             name: 'page1',
@@ -8,52 +8,61 @@ const SURVEY_DATA = {
                     type: 'radiogroup',
                     choices: ['Yes', 'No'],
                     isRequired: true,
-                    name: 'skiptoend',
+                    name: 'skipToEnd',
                     title: 'Would you like to skip to the end?',
                 },
                 {
                     type: 'radiogroup',
                     choices: ['Yes', 'No'],
-                    isRequired: false,
-                    name: 'frameworkUsing',
-                    title: 'Do you use any front-end framework like Bootstrap?',
+                    isRequired: true,
+                    name: 'coloradoArrest',
+                    title: 'Did your arrest or charge take place in Colorado?',
                 },
                 {
-                    type: 'checkbox',
-                    choices: ['Bootstrap', 'Foundation'],
-                    hasOther: true,
+                    type: 'radiogroup',
+                    choices: ['Yes', 'No'],
                     isRequired: true,
-                    name: 'framework',
-                    title: 'What front-end framework do you use?',
-                    visibleIf: "{frameworkUsing} = 'Yes'",
+                    name: 'over18',
+                    title: 'Were you over 18 at the time of the arrest or charge?',
+                    visibleIf: "{coloradoArrest} = 'Yes'",
+                },
+                {
+                    type: 'radiogroup',
+                    choices: ['Yes', 'No', "Don't Know/Not Sure"],
+                    isRequired: true,
+                    name: 'federalOrMunicipal',
+                    title: 'Was your case a federal or muncipal case?',
+                    visibleIf: "{over18} = 'Yes'",
                 },
             ],
         },
         {
             name: 'page2',
-            visibleIf: "{skiptoend} = 'No'",
+            visibleIf:
+                "{skipToEnd} = 'No' and {coloradoArrest} = 'Yes' and {over18} = 'Yes' and {federalOrMunicipal} != 'Yes'",
             questions: [
                 {
                     type: 'radiogroup',
                     choices: ['Yes', 'No'],
                     isRequired: true,
-                    name: 'mvvmUsing',
-                    title: 'Do you use any MVVM framework?',
+                    name: 'sealingArrestRecordOnly',
+                    title:
+                        'Were you arrested, but not charged & seeking to seal arrest record only?',
                 },
                 {
-                    type: 'checkbox',
-                    choices: ['AngularJS', 'KnockoutJS', 'React'],
-                    hasOther: true,
+                    type: 'radiogroup',
+                    choices: ['Yes', 'No'],
                     isRequired: true,
-                    name: 'mvvm',
-                    title: 'What MVVM framework do you use?',
-                    visibleIf: "{mvvmUsing} = 'Yes'",
+                    name: 'caseDismissedOrAcquitted',
+                    title:
+                        'Were all charges in your case dismissed or were you acquitted (found not guilty)?',
+                    visibleIf: "{sealingArrestRecordOnly} = 'No'",
                 },
             ],
         },
         {
             name: 'page3',
-            visibleIf: "{skiptoend} = 'No'",
+            visibleIf: "{skipToEnd} = 'Yes'",
             questions: [
                 {
                     type: 'comment',
@@ -63,7 +72,43 @@ const SURVEY_DATA = {
                 },
             ],
         },
+        {
+            name: 'page4',
+            visibleIf:
+                "{coloradoArrest} = 'No' or {over18} = 'No' or {federalOrMunicipal} = 'Yes'",
+            questions: [
+                {
+                    type: 'comment',
+                    name: 'about',
+                    title:
+                        'Given your responses, your case is not eligible for the services provided by Expunge Colorado.',
+                },
+            ],
+        },
+        {
+            name: 'page5',
+            visibleIf:
+                "{sealingArrestRecordOnly} = 'Yes' or {caseDismissedOrAcquitted} = 'Yes'",
+            questions: [
+                {
+                    type: 'comment',
+                    name: 'about',
+                    title:
+                        'Given your responses, your case is eligible for the services provided by Expunge Colorado. Please provide your contact information in the space below',
+                },
+            ],
+        },
     ],
 };
+
+// {
+//     type: 'checkbox',
+//     choices: ['Bootstrap', 'Foundation'],
+//     hasOther: true,
+//     isRequired: true,
+//     name: 'framework',
+//     title: 'What front-end framework do you use?',
+//     visibleIf: "{frameworkUsing} = 'Yes'",
+// },
 
 export default SURVEY_DATA;
