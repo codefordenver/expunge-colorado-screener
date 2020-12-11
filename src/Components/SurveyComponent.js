a85a34c8182f6d99efafe91f1885ed91b18866bc
Isadora Decker-Lucke <isadoradl5@gmail.com>
2020-12-10 18:48:44 -0700

    added basic css styling and setup

--- src/Components/SurveyComponent.js
+++ src/Components/SurveyComponent.js
@@ -3,25 +3,21 @@ import * as Survey from 'survey-react';
 import SURVEY_DATA from '../data/survey.js';
 import { useState, useEffect, useRef } from 'react';
 
+const myCss = {
+    matrix: {
+        root: 'table table-striped',
+    },
+    navigationButton: 'btn-nav',
+    header: 'header',
+    container: 'container',
+};
+
 function SurveyComponent() {
     function sendDataToServer(survey) {
         console.log(survey.data);
     }
 
-    useEffect(() => {
-        const defaultThemeColors = Survey.StylesManager.ThemeColors['default'];
-        defaultThemeColors['$main-color'] = 'red';
-        defaultThemeColors['$main-hover-color'] = 'green';
-        defaultThemeColors['$text-color'] = 'yellow';
-        defaultThemeColors['$header-color'] = 'pink';
-
-        defaultThemeColors['$header-background-color'] = '#4a4a4a';
-        defaultThemeColors['$body-container-background-color'] = '#f8f8f8';
-
-        Survey.StylesManager.applyTheme();
-    }, []);
-
-    return <Survey.Survey json={SURVEY_DATA} onComplete={sendDataToServer} />;
+    return <Survey.Survey json={SURVEY_DATA} css={myCss} onComplete={sendDataToServer} />;
 }
 
 export default SurveyComponent;
--- src/index.js
+++ src/index.js
@@ -5,8 +5,11 @@ import './App.scss';
 
 ReactDOM.render(
     <React.StrictMode>
-        <div className="center-me">
-            <SurveyComponent />
+        <div className="app">
+            <h1>Expunge Colorado Screener</h1>
+            <div className="center-me">
+                <SurveyComponent />
+            </div>
         </div>
     </React.StrictMode>,
     document.getElementById('root')
