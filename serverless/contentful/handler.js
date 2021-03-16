66faa07b623d93a79d0477d6369ca2892020b5ad
Michelle Lim <elle@mmmanyfold.com>
2021-03-15 19:07:04 -0600

    contentful serverless handler returns entries

--- serverless/contentful/handler.js
+++ serverless/contentful/handler.js
@@ -1,18 +1,32 @@
 'use strict';
+const contentful = require('contentful');
+
+const SPACE_ID = 'rlnzyzpg3lzx';
+const ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
+
+const client = contentful.createClient({
+    space: SPACE_ID,
+    accessToken: ACCESS_TOKEN,
+});
+
+const getOutcomes = async () => {
+    try {
+        const response = await client.getEntries();
+        return {
+            statusCode: 200,
+            body: JSON.stringify(response.items, null, 2),
+        };
+    } catch (e) {
+        // TODO: return
+    }
+};
+
+const mapAssetsToEntries = (entries, assets) => {
+    // TODO?
+};
 
 module.exports.contentful = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+    return await getOutcomes();
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
