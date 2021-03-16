06abcaa60979b6bbb8d092908bbf687d4332b182
Kristine Du <kristine.a.du@gmail.com>
2021-03-15 20:44:49 -0600

    Fetch from contentful (#70)
    * init contentful lambda service
    
    * add iamRoleStatements
    
    * rename function hello -> contentful
    
    * update region
    
    * prettier for serverless dir, add contentful dep
    
    * contentful serverless handler returns entries
    
    * Add CORS headers
    
    * Update api to get content from API gateway endpoint
    
    * Update api to get content from API gateway endpoint
    
    Co-authored-by: Michelle Lim <elle@mmmanyfold.com>

---
+++ serverless/contentful/handler.js
@@ -0,0 +1,37 @@
+'use strict';
+const contentful = require('contentful');
+
+const SPACE_ID = 'rlnzyzpg3lzx';
+const ACCESS_TOKEN = process.env.CONTENTFUL_ACCESS_TOKEN;
+
+const client = contentful.createClient({
+    space: SPACE_ID,
+    accessToken: ACCESS_TOKEN,
+});
+
+const getOutcomes = async () => {
+    try {
+        const response = await client.getEntries();
+        return {
+            statusCode: 200,
+            headers: {
+                'Access-Control-Allow-Headers': 'Content-Type',
+                'Access-Control-Allow-Origin': '*',
+                'Access-Control-Allow-Methods': 'OPTIONS,GET',
+            },
+            body: JSON.stringify(response.items, null, 2),
+        };
+    } catch (e) {
+        // TODO: return
+    }
+};
+
+const mapAssetsToEntries = (entries, assets) => {
+    // TODO?
+};
+
+module.exports.contentful = async (event) => {
+    return await getOutcomes();
+    // Use this code if you don't use the http event with the LAMBDA-PROXY integration
+    // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
+};
