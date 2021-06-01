8df146d1ba044bafe4eff8d4e9f8fc0dc871da21
jdhgit <17481018+jdhern17@users.noreply.github.com>
2021-06-01 00:36:46 -0600

    updated report serverless handler.js with code from testScan in aws console

--- serverless/report/handler.js
+++ serverless/report/handler.js
@@ -1,25 +1,37 @@
 'use strict';
 const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
+const fs = require('fs');
 
-// const headers = {
-//     'Access-Control-Allow-Origin': '*',
-//     'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
-//     'Access-Control-Allow-Headers': 'Content-Type',
-// };
-
-module.exports.report = async (event) => {
+exports.handler = async (event) => {
     try {
         //performing scan
         let params = {
             TableName: 'expunge-survey-results',
-            Limit: 1000
+            Limit: 10
         };
         let result = await docClient.scan(params).promise();
         console.log('Query Success');
+        let resultCsv = "initial,table,cols\r";
         result.Items.forEach((element) => {
-            console.log(element);
+            // for each field, append content + ','
+            // object.keys?
+            resultCsv += element.uuid + ",";
+            resultCsv += element.coloradoArrest + ",";
+            resultCsv += element.outcome + ",";
+            // at the end of each field appending, append \r
+            resultCsv += "\r";
         });
+        console.log("this is the csv before write");
+        console.log(resultCsv);
+        // fs.writeFile resultCsv to file
+        fs.writeFile("./resultCsv.csv", resultCsv, (err) => {
+            if (err) {
+                console.log('this is the filewrite error');
+                console.log(err)
+            };
+            console.log("this is the successful data response from fs");
+        })
         return {
             // headers,
             // statusCode: 200,
