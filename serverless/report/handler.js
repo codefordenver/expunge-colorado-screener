ab53c11b95c11d535595cdaa735101d1feb65d52
jdhgit <17481018+jdhern17@users.noreply.github.com>
2021-06-03 19:35:08 -0600

    added preliminary s3 code to serverless and lambda handler

--- serverless/report/handler.js
+++ serverless/report/handler.js
@@ -1,7 +1,7 @@
 'use strict';
 const AWS = require('aws-sdk');
 const docClient = new AWS.DynamoDB.DocumentClient();
-const fs = require('fs');
+const s3 = new AWS.S3();
 
 exports.handler = async (event) => {
     try {
@@ -24,14 +24,18 @@ exports.handler = async (event) => {
         });
         console.log("this is the csv before write");
         console.log(reportCsv);
-        // fs.writeFile reportCsv to file
-        fs.writeFile("./reportCsv.csv", reportCsv, (err) => {
-            if (err) {
-                console.log('this is the filewrite error');
-                console.log(err)
-            };
-            console.log("this is the successful data response from fs");
-        })
+        let bucket = "cfdreportbucket";
+        let key = "report.csv";
+        let body = JSON.stringify(reportCsv);
+        let s3Params = {
+            Bucket : bucket,
+            Key : key,
+            Body : data
+        }
+        s3.putObject(s3Params, function(err, data) {
+          if (err) console.log(err, err.stack); // an error occurred
+          else     console.log(data);           // successful response
+        });
         return {
             // headers,
             // statusCode: 200,
