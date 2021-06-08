f03522adbafc7b7a38562209b56a3f31b8ad3864
jdhgit <17481018+jdhern17@users.noreply.github.com>
2021-06-07 23:36:14 -0600

    using async/await syntax for s3.upload

--- serverless/report/handler.js
+++ serverless/report/handler.js
@@ -8,34 +8,38 @@ exports.handler = async (event) => {
         //performing scan
         let params = {
             TableName: 'expunge-survey-results',
-            Limit: 10
+            Limit: 1000
         };
         let result = await docClient.scan(params).promise();
         console.log('Query Success');
-        let reportCsv = "initial,table,cols\r";
+        let reportCsv = "initial,table,cols\n";
         result.Items.forEach((element) => {
             // for each field, append content + ','
             // object.keys?
             reportCsv += element.uuid + ",";
             reportCsv += element.coloradoArrest + ",";
             reportCsv += element.outcome + ",";
-            // at the end of each field appending, append \r
-            reportCsv += "\r";
+            // at the end of each field appending, append \n
+            reportCsv += "\n";
         });
         console.log("this is the csv before write");
         console.log(reportCsv);
         let bucket = "cfdreportbucket";
-        let key = "report.csv";
-        let body = JSON.stringify(reportCsv);
+        let todayDate = new Date();
+        let timestamp = todayDate.getFullYear() + ("0" + (todayDate.getMonth() + 1)).slice(-2) + ("0" + todayDate.getDate()).slice(-2) + ("0" + todayDate.getHours() + 1 ).slice(-2) + ("0" + todayDate.getMinutes()).slice(-2) + ("0" + todayDate.getSeconds()).slice(-2);
+        let key = `report${timestamp}.csv`;
         let s3Params = {
             Bucket : bucket,
             Key : key,
-            Body : data
+            Body : reportCsv,
+            ContentType: 'text/csv'
         }
-        s3.putObject(s3Params, function(err, data) {
-          if (err) console.log(err, err.stack); // an error occurred
-          else     console.log(data);           // successful response
-        });
+        try {
+            let s3Response = await s3.upload(s3Params).promise();
+            console.log('put success', s3Response.Location);
+        } catch (e){
+            console.error('s3 put failure. Error', e);
+        }
         return {
             // headers,
             // statusCode: 200,
